5fe9cd1b6c834391c962544ee41ba4ce972caff7
Skullyfox <Skullyfox.yt@gmail.com>
2019-09-07 06:00:49 +0200

    Navigation + adding of FontAwesome 5.10.2

--- components/nav.js
+++ components/nav.js
@@ -1,56 +1,51 @@
 import React from 'react'
 import Link from 'next/link'
+import Logo from '../static/logoNav.png';
+import UserPic from '../static/user.png';
 
-const links = [
-  { href: 'https://zeit.co/now', label: 'ZEIT' },
-  { href: 'https://github.com/zeit/next.js', label: 'GitHub' }
-].map(link => {
-  link.key = `nav-link-${link.href}-${link.label}`
-  return link
-})
 
-const Nav = () => (
-  <nav>
-    <ul>
-      <li>
-        <Link href='/'>
-          <a>Home</a>
-        </Link>
-      </li>
-      {links.map(({ key, href, label }) => (
-        <li key={key}>
-          <a href={href}>{label}</a>
+function Nav() {
+  return(
+    <nav className="nav">
+      <ul className="navItems">
+        <li className="item">
+          <img src={Logo} alt="Logo Horizons" />
         </li>
-      ))}
-    </ul>
-
-    <style jsx>{`
-      :global(body) {
-        margin: 0;
-        font-family: -apple-system, BlinkMacSystemFont, Avenir Next, Avenir,
-          Helvetica, sans-serif;
-      }
-      nav {
-        text-align: center;
-      }
-      ul {
-        display: flex;
-        justify-content: space-between;
-      }
-      nav > ul {
-        padding: 4px 16px;
-      }
-      li {
-        display: flex;
-        padding: 6px 8px;
-      }
-      a {
-        color: #067df7;
-        text-decoration: none;
-        font-size: 13px;
-      }
-    `}</style>
-  </nav>
-)
+        <li className="item">
+          <ul className="itemLinks">
+            <li className="Link">
+              <Link href='/'>
+                <a>Présentation</a>
+              </Link>
+            </li>
+            <li className="Link">
+              <Link href='/'>
+                <a>News</a>
+              </Link>
+            </li>
+            <li className="Link">
+              <Link href='/'>
+                <a>Sections</a>
+              </Link>
+            </li>
+            <li className="Link">
+              <Link href='/'>
+                <a>Évennements</a>
+              </Link>
+            </li>
+            <li className="Link">
+              <Link href='/'>
+                <a>Streams</a>
+              </Link>
+            </li>
+            <li className="Link">
+              <img src={UserPic} alt="" />
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </nav>
+  )
+}
 
 export default Nav
